693f331f5e4166abc805ccf30c6659176d5b82e8
Connor GOddard <clg11@aber.ac.uk>
2013-04-25 08:55:05 +0100

    IMplemented lottery scheduling algorithm

--- CS221-Assignment_2/src/uk/ac/aber/rcs/cs211/schedulersim/scheduler/Lottery.java
+++ CS221-Assignment_2/src/uk/ac/aber/rcs/cs211/schedulersim/scheduler/Lottery.java
@@ -1,9 +1,42 @@
 package uk.ac.aber.rcs.cs211.schedulersim.scheduler;
 
+import java.util.Random;
+
 import uk.ac.aber.rcs.cs211.schedulersim.AbstractScheduler;
+import uk.ac.aber.rcs.cs211.schedulersim.Job;
 
 public class Lottery extends AbstractScheduler {
 	
+	private Random randGen;
 	
+	public Lottery() {
+		randGen = new Random();
+	}
+	
+	private void runLottery() throws SchedulerException {
+		
+		int token = randGen.nextInt(this.numberOfJobs-0) + 0;
+		
+		Job tempJob = queue.get(token);
+		
+		removeJob(tempJob);
+		
+		queue.add(0, tempJob);
+		numberOfJobs++;
+			
+	}
+	
+	public void addNewJob(Job job) throws SchedulerException {
+		if (this.queue.contains(job)) throw new SchedulerException("Job already on Queue");
+		
+		this.queue.add(this.numberOfJobs, job);
+		this.numberOfJobs++;
+		runLottery();
+	}
+	
+	public void returnJob(Job job) throws SchedulerException {
+		if (!this.queue.contains(job)) throw new SchedulerException("Job not on Queue");
+		runLottery();
+	}
 
 }
